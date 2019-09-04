c81afaddf52ae876d9e739405003d033ffba16ec
Austin Smith <trunk9slamchest@gmail.com>
2019-09-04 00:59:09 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,14 +1,17 @@
 require 'yaml'
 
 def load_library(file_path)
+  
   load_yaml = YAML.load_file(file_path)
 
 	japanese = {}
 	english = {}
 
 	load_yaml.reduce({}) { |memo, (meaning, emoticons)|
-	japanese[emoticons[1]] = meaning
-	english[emoticons[0]] = emoticons[1]
+	
+  	japanese[emoticons[1]] = meaning
+  	english[emoticons[0]] = emoticons[1]
+  	
 	}
 
 	hash = {:get_meaning => japanese, :get_emoticon => english}
@@ -16,19 +19,27 @@ def load_library(file_path)
 return hash
 end
 
-def get_japanese_emoticon(file_path, emoticon)
-  val1 = load_library(file_path).reduce({}) { |x, (y, z) |
-		find = z.select { |k| k == emoticon
 
-		}
-x = find.values[0]
-if x == nil
-x = "Sorry, that emoticon was not found"
+
+def get_japanese_emoticon(file_path, emoticon)
+
+  search = load_library(file_path).reduce({}) { | memo, (main_keys, values) |
+
+		find_emoticon = values.select { |key| key == emoticon }
+
+		memo = find_emoticon.values[0]
+
+		if memo == nil
+			memo = "Sorry, that emoticon was not found"
+		end
+
+	memo
+	}
+
+return search
 end
-x
-  }
-return val1
-end
+
+
 
 def get_english_meaning
   # code goes here
